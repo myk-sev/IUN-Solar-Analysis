6dbb3014251b123f5e2242875f7fe86aece7f9d2
myk-sev <sev.myk@outlook.com>
2025-04-24 01:45:30 -0500

    back up commit: rebase completed

--- category_graphs.R
+++ category_graphs.R
@@ -1,7 +1,9 @@
 library(lubridate)
 library(readr)
+library(ggplot2)
 
 closest_hour <- function(time) {
+  #All data is recorded in two hour intervals. This finds the closest interval.
   if (is.na(time)) { #handles missing enteries
     return(as_datetime(0)) #ensures all enteries have the same type
   }
@@ -12,7 +14,7 @@ closest_hour <- function(time) {
   }
 }
 
-plot_solar_noon <- function(dataDF) {
+extract_avg_solar_noons <- function(dataDF) {
   avgDF <- data.frame(Time = unique(dataDF$Hour), Flat=0, Angled=0)
   for (hourValue in avgDF$Time) {
     hourEnteries <- subset(dataDF, dataDF$Hour == hourValue)
@@ -30,6 +32,27 @@ plot_solar_noon <- function(dataDF) {
   fullSunAvgDF
   order(fullSunAvgDF[[1]])
   fullSunAvgDF[order(fullSunAvgDF[[1]]),]
+  noonDF <- subset(avgDF, hour(avgDF$Time) == 12)
+  return(noonDF)
+}
+
+ggplot_solar_noon <- function(dataDF) {
+  avgDF <- data.frame(Time = unique(dataDF$Hour), Flat=0, Angled=0)
+  for (hourValue in avgDF$Time) {
+    hourEnteries <- subset(dataDF, dataDF$Hour == hourValue)
+    flatAvg <- mean(hourEnteries$Flat, na.rm=TRUE)
+    angledAvg <- mean(hourEnteries$Angled, na.rm=TRUE)
+    avgDF$Flat[avgDF$Time == hourValue] = flatAvg
+    avgDF$Angled[avgDF$Time == hourValue] = angledAvg
+  }
+  noonDF <- subset(avgDF, hour(avgDF$Time) == 12)
+  ggplot(
+    data = noonDF,
+    mapping = aes(x=Time, y=Flat)
+  ) + 
+  geom_point() +
+  geom_line()
+
 }
 
 locationsDF <- read_csv("location_details.csv", col_types = cols(
@@ -71,3 +94,26 @@ partialShadeDF <- subset(relevantDF, relevantDF$Category == "Partial Shade")
 plot_solar_noon(fullSunDF)
 plot_solar_noon(fullShadeDF)
 plot_solar_noon(partialShadeDF)
+
+avgFullShadeNoonDF <- extract_avg_solar_noons(fullShadeDF)
+avgFullSunNoonDF <- extract_avg_solar_noons(fullSunDF)
+avgPartialShadeNoonDF <- extract_avg_solar_noons(partialShadeDF)
+
+avgFullShadeNoonDF$Category = "Full Shade"
+avgFullSunNoonDF$Category = "Full Sun"
+avgPartialShadeNoonDF$Category = "Partial Shade"
+
+allNoonsDF <- rbind(avgFullShadeNoonDF, avgFullSunNoonDF, avgPartialShadeNoonDF)
+
+ggplot(
+  data = allNoonsDF,
+  mapping = aes(x=Time, y=Flat, color=Category)
+) + 
+  geom_point() +
+  geom_line(linewidth = 1.75) +
+  labs(
+    color="Location Type", 
+    x="Date", 
+    y=expression("Solar Irradiance (W/m"^2*")"),
+    title= "Measured Solar Irradiance By Location Type"
+    )
