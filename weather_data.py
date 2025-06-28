11a0f08a0ff3d2121fb1f327dabc8c2e961563dd
myk-sev <sev.myk@outlook.com>
2025-06-28 12:32:24 -0500

    both data sources aligned onto datatime object common format

--- weather_data.py
+++ weather_data.py
@@ -1,12 +1,29 @@
 import requests
 import pandas as pd
 import datetime
-from urllib.parse import quote
 import dotenv
 import os
 
 DAILY_DATA_FILENAME = "daily_data.csv"
 CORE_ADDRESS = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
+
+def get_cloud_cover(month, day, year, hour):
+    params = {
+        "key": os.environ.get("API_KEY"),
+        "contentType": os.environ.get("OUTPUT_TYPE"),
+        "elements": os.environ.get("DATA"),
+    }
+    zipcode = os.environ.get("ZIPCODE")
+    date = f"{year}-{month}-{day}"
+
+    response = requests.get(url=f"{CORE_ADDRESS}/{zipcode}/{date}", params=params)
+    data = response.json()["days"]
+    for entry in data:
+        dtobj = datetime.fromtimestamp(entry["datetimeEpoch"])
+        if dtobj.hour == hour:
+            return entry
+
+
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
@@ -17,10 +34,25 @@ if __name__ == "__main__":
         "elements": os.environ.get("DATA"),
     }
 
-    dailyDF = pd.read_csv(DAILY_DATA_FILENAME)
-    dailyDF.head()
+    solarDailyDF = pd.read_csv(DAILY_DATA_FILENAME)
 
     zipcode = os.environ.get("ZIPCODE")
     date = os.environ.get("DATE")
     response = requests.get(url = f"{CORE_ADDRESS}/{zipcode}/{date}", params=params)
+    relevantData = response.json()["days"][0]["hours"]
+    visualCrossingDF = pd.DataFrame(relevantData)
+
+    #The general workflow for working with time data is to keep time in the datatime obj format, converting to and from the various types as needed.
+    #This includes Unix type and string formats.
+    visualCrossingDF["datetimeEpoch"] = visualCrossingDF["datetimeEpoch"].apply(lambda entry: datetime.fromtimestamp(entry))
+    visualCrossingDF.rename(columns={"datetimeEpoch": "Date Time"}, inplace=True)
+
+    solarDailyDF["Date Time"] = pd.to_datetime(solarDailyDF["Date"] + "T" + solarDailyDF["Time"], format="%Y-%m-%dT%H:%M")
+
+    
+
+
+
+
+
 
