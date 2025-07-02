6bb7f87aea95e9ec5ecca0b31ae2e3f25c5991ec
myk-sev <sev.myk@outlook.com>
2025-07-02 10:24:07 -0500

    clean up & cloudcover extraction fix

--- weather_data.py
+++ weather_data.py
@@ -3,26 +3,26 @@ import pandas as pd
 import datetime
 import dotenv
 import os
+import time
 
 DAILY_DATA_FILENAME = "daily_data.csv"
 CORE_ADDRESS = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
 
-def get_cloud_cover(month, day, year, hour):
-    params = {
-        "key": os.environ.get("API_KEY"),
-        "contentType": os.environ.get("OUTPUT_TYPE"),
-        "elements": os.environ.get("DATA"),
-    }
-    zipcode = os.environ.get("ZIPCODE")
-    date = f"{year}-{month}-{day}"
+def to_nearest_interval(dtobj):
+    """
+    Modifies datetime object to align with nearest two hour interval.
 
-    response = requests.get(url=f"{CORE_ADDRESS}/{zipcode}/{date}", params=params)
-    data = response.json()["days"]
-    for entry in data:
-        dtobj = datetime.fromtimestamp(entry["datetimeEpoch"])
-        if dtobj.hour == hour:
-            return entry
+    Parameters:
+        dtobj (datetime): The original datetime object.
 
+    Returns:
+        datetime: A new datetime object with the modified time.
+    """
+    if dtobj.hour % 2 == 0: #round down if even
+        return dtobj.replace(minute=0)
+
+    else: #round up if odd
+        return dtobj.replace(hour=dtobj.hour+1, minute=0)
 
 
 if __name__ == "__main__":
@@ -33,38 +33,27 @@ if __name__ == "__main__":
         "contentType": os.environ.get("OUTPUT_TYPE"),
         "elements": os.environ.get("DATA"),
     }
+    zipcode = os.environ.get("ZIPCODE")
 
     solarDailyDF = pd.read_csv(DAILY_DATA_FILENAME)
 
-    zipcode = os.environ.get("ZIPCODE")
-    date = os.environ.get("DATE")
-    response = requests.get(url = f"{CORE_ADDRESS}/{zipcode}/{date}", params=params)
-    relevantData = response.json()["days"][0]["hours"]
-    visualCrossingDF = pd.DataFrame(relevantData)
-
     #The general workflow for working with time data is to keep time in the datatime obj format, converting to and from the various types as needed.
     #This includes Unix type and string formats.
-    visualCrossingDF["datetimeEpoch"] = visualCrossingDF["datetimeEpoch"].apply(lambda entry: datetime.fromtimestamp(entry))
-    visualCrossingDF.rename(columns={"datetimeEpoch": "Date Time"}, inplace=True)
+    solarDailyDF["Date Time"] = pd.to_datetime(solarDailyDF["Date"] + "T" + solarDailyDF["Time"], format="%Y-%m-%dT%H:%M") #convert text enteries to datetime obj
 
-    solarDailyDF["Date Time"] = pd.to_datetime(solarDailyDF["Date"] + "T" + solarDailyDF["Time"], format="%Y-%m-%dT%H:%M")
+    solarDailyDF["Date Time"] = solarDailyDF["Date Time"].apply(lambda entry: to_nearest_interval(entry))
+    daysToRetrieve = solarDailyDF["Date Time"].dt.strftime("%Y-%m-%d").unique() #convert dtobj to visualcrossing format and find unique enteries
 
-    daysToRetrieve = solarDailyDF["Date Time"].dt.strftime("%Y-%m-%d").unique()
     relevantData = []
     for day in daysToRetrieve:
+        print(day)
         response = requests.get(url=f"{CORE_ADDRESS}/{zipcode}/{day}", params=params)
         relevantData += response.json()["days"][0]["hours"]
 
     visualCrossingDF = pd.DataFrame(relevantData)
     visualCrossingDF["datetimeEpoch"] = visualCrossingDF["datetimeEpoch"].apply(
-        lambda entry: datetime.fromtimestamp(entry))
-    visualCrossingDF.rename(columns={"datetimeEpoch": "Date Time"}, inplace=True)
+        lambda entry: datetime.datetime.fromtimestamp(entry))  #convert from unix to datetime obj
+    visualCrossingDF.rename(columns={"datetimeEpoch": "Date Time"}, inplace=True) #rename the column to match the new format
 
     solarDailyDF["cloudcover"] = visualCrossingDF["cloudcover"][
-        visualCrossingDF["Date Time"].isin(solarDailyDF["Date Time"])]
-
-
-
-
-
-
+        visualCrossingDF["Date Time"].isin(solarDailyDF["Date Time"])].values
